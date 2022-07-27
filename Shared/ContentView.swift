70a2fb4ce56ee280a6f4eb9327f9ee638f0639a4
Zephyr Xyd Espino <101380933+xydespino@users.noreply.github.com>
2022-07-27 16:41:39 -0300

    Added default Xcode Files

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Xyd on 2022-07-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Zephyr!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MatchItXCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchItXCodeApp.swift
+//  Shared
+//
+//  Created by Xyd on 2022-07-27.
+//
+
+import SwiftUI
+
+@main
+struct MatchItXCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
