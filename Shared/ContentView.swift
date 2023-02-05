82a441a2ba8dfabb5757ae26c4d9c62961178fc1
Zephyr Xyd Espino <zp270900@dal.ca>
2023-02-05 01:46:29 -0400

    Lecture 2: Learning more about SwiftUI

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,40 +7,118 @@
 
 import SwiftUI
 
+
+// Contents of the screen
 struct ContentView: View {
-    //    var i: Int
-    //    var s: String
-    //    var counter: Int
-    //    counter = 100;
-    
+    @State var emojis = ["💀","😉","😈","🥲","🤣","🥹"]
+    @State var emojiCount = 6
     var body: some View {
-        return ZStack(content: {
-            RoundedRectangle(cornerRadius: 25)
-                .stroke(lineWidth: 5)
-                .foregroundColor(.gray)
-                .padding(.horizontal)
-            Text("Hello, Xyd!")
-                .padding()
-                .foregroundColor(Color.orange)
+        VStack{
+            // Emoji cards
             
+            LazyVGrid(columns: [GridItem(),GridItem(),GridItem()]){
+//            HStack{
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+            }
+            Spacer()
+            
+            // Remove / Add Options
+            HStack{
+                remove
+                Spacer()
+                LikeButton()
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+        }.padding(.all)
+    }
+    
+    
+    // Remove Card Button
+    var remove: some View {
+        Button(action: {
+            if(emojiCount > 1){
+                emojiCount -= 1
+            }
+        }, label : {
+            VStack{
+                Text("Remove")
+                Text("Card")
+            }
         })
-        
-
-
+    }
+    
+    
+    // Add Card Button
+    var add: some View {
+        Button(action: {
+            if(emojiCount < emojis.count){
+                emojiCount += 1
+            }
+        }, label : {
             
-        
-        
-        
-    
-
-    
-    
-        
+            VStack{
+                
+                Text("Add")
+                Text("Card")
+            }
+        })
     }
 }
 
+
+// Card structure
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 25)
+            if isFaceUp{
+            // Outlined rectangle on top of filled rectangle
+                shape.foregroundColor(.white)
+                shape.stroke(lineWidth: 5).foregroundColor(.gray)
+                Text(content).foregroundColor(.orange).font(.largeTitle)
+            } else {
+            // Back of card
+                shape.foregroundColor(.green)
+            }
+        }
+        .onTapGesture{
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+//
+struct LikeButton: View {
+    @State var isLiked : Bool = false;
+    var body: some View{
+        ZStack{
+            if(isLiked){
+                Image(systemName: "heart.fill")
+            } else {
+                Image(systemName: "heart")
+            }
+        }.onTapGesture {
+            isLiked = !isLiked
+        }
+        .foregroundColor(.red)
+    }
+}
+
+// Preview Function
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+
     }
 }
