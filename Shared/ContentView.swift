47b3f5b338a78bda3abf86f34dc5809e08620ab5
Zephyr Xyd Espino <zp270900@dal.ca>
2023-02-23 21:30:23 -0400

    Adding Assignment 1 progress and Lecture 3 additions

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,67 +10,76 @@ import SwiftUI
 
 // Contents of the screen
 struct ContentView: View {
-    @State var emojis = ["💀","😉","😈","🥲","🤣","🥹"]
-    @State var emojiCount = 6
+    @State var emojis = ["💀","😉","😈","🥲","🤣","🥹","🤤","🤯","😰","😶‍🌫️","🎃","🫶","👄","👀"]
+    @State var emojiCount = 14
     var body: some View {
         VStack{
+            title
             // Emoji cards
-            
-            LazyVGrid(columns: [GridItem(),GridItem(),GridItem()]){
-//            HStack{
-                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                    CardView(content: emoji)
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]){
+                    //HStack{
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
                 }
             }
-            Spacer()
-            
-            // Remove / Add Options
-            HStack{
-                remove
-                Spacer()
-                LikeButton()
-                Spacer()
-                add
-            }
-            .font(.largeTitle)
-            .padding(.horizontal)
-        }.padding(.all)
-    }
-    
-    
-    // Remove Card Button
-    var remove: some View {
-        Button(action: {
-            if(emojiCount > 1){
-                emojiCount -= 1
-            }
-        }, label : {
-            VStack{
-//                Text("Remove")
-//                Text("Card")
-                Image(systemName: "minus")
-            }
-        })
-    }
-    
-    
-    // Add Card Button
-    var add: some View {
-        Button(action: {
-            if(emojiCount < emojis.count){
-                emojiCount += 1
-            }
-        }, label : {
-            
-            VStack{
-                
-//                Text("Add")
-//                Text("Card")
-                Image(systemName: "plus")
-            }
-        })
+                //Spacer()
+                // Remove / Add Options
+//                HStack{
+//                    remove
+//                    Spacer()
+//                    LikeButton()
+//                    Spacer()
+//                    add
+//                }
+//                .font(.largeTitle)
+//                .padding(.horizontal)
+            }.padding(.all)
+        
     }
 }
+    
+//    Title
+var title: some View{
+    Text("Memorize!")
+        .font(.largeTitle)
+        .fontWeight(.bold)
+        .foregroundColor(Color.red)
+}
+//    // Remove Card Button
+//    var remove: some View {
+//        Button(action: {
+//            if(emojiCount > 1){
+//                emojiCount -= 1
+//            }
+//        }, label : {
+//            VStack{
+//                Text("Remove")
+//                Text("Card")
+//                Image(systemName: "minus")
+//            }
+//        })
+//    }
+//
+//
+//    // Add Card Button
+//    var add: some View {
+//        Button(action: {
+//            if(emojiCount < emojis.count){
+//                emojiCount += 1
+//            }
+//        }, label : {
+//
+//            VStack{
+//
+//                Text("Add")
+//                Text("Card")
+//                Image(systemName: "plus")
+//            }
+//        })
+//    }
+//}
 
 
 // Card structure
@@ -83,7 +92,7 @@ struct CardView: View {
             if isFaceUp{
             // Outlined rectangle on top of filled rectangle
                 shape.foregroundColor(.white)
-                shape.stroke(lineWidth: 5).foregroundColor(.gray)
+                shape.strokeBorder(lineWidth: 5).foregroundColor(.gray)
                 Text(content).foregroundColor(.orange).font(.largeTitle)
             } else {
             // Back of card
@@ -119,6 +128,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .preferredColorScheme(.dark)
+            .previewInterfaceOrientation(.portraitUpsideDown)
         ContentView()
             .preferredColorScheme(.light)
 
---
+++ Shared/MemoryGame.swift
@@ -0,0 +1,22 @@
+//
+//  MemoryGame.swift
+//  MatchItXCode (iOS)
+//
+//  Created by Xyd on 2023-02-23.
+//
+
+import Foundation
+
+//struct MemoryGame{
+//    var cards: Array<Card>
+//    
+//    func choose(card: Card){
+//        
+//        
+//        
+//        
+//        
+//    }
+//}
+
+
